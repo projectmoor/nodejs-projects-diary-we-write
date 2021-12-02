ff276810ecff879b2824f5a29878b993cbc49472
pannshenn <pannshenn@gmail.com>
2021-12-02 10:14:03 -0500

    modified express static path

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const findOrCreate = require('mongoose-findorcreate');
 const app = express();
 
 // const path = require('path');
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + 'public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
